407c33471918b8007b5097d2ec695044da122546
Ben Galewsky <bengal1@illinois.ed>
2019-03-05 16:04:13 -0600

    Working zPeak

--- demo/play.py
+++ demo/play.py
@@ -1,3 +1,4 @@
+from pyspark.sql import functions
 from pyspark.sql.functions import pandas_udf, PandasUDFType
 from pyspark.accumulators import AccumulatorParam
 from pyspark.shell import spark
@@ -12,6 +13,7 @@ import fnal_column_analysis_tools.hist as hist
 from pyspark.sql.types import IntegerType
 
 import aghast.connect.numpy as connect_numpy
+from fnal_column_analysis_tools.analysis_objects import JaggedCandidateArray
 
 
 class FLNAL_Hist_AccumulatorParam(AccumulatorParam):
@@ -25,18 +27,18 @@ class FLNAL_Hist_AccumulatorParam(AccumulatorParam):
 
 hists = OrderedDict()
 
-dataset = hist.Cat("dataset", "DAS name")
-channel_cat = hist.Cat("channel", "dilepton flavor")
+dataset_axis = hist.Cat("dataset", "DAS name")
+channel_cat_axis = hist.Cat("channel", "dilepton flavor")
 
 # underflow = negative weight sum, overflow = positive weight sum
-hists['genw'] = hist.Hist("Events", dataset, hist.Bin("genw", "Gen weight", [0.]))
+hists['genw'] = hist.Hist("Events", dataset_axis, hist.Bin("genw", "Gen weight", [0.]))
 
-hists['lepton_pt'] = hist.Hist("Events", dataset, channel_cat,
+hists['lepton_pt'] = hist.Hist("Events", dataset_axis, channel_cat_axis,
                                hist.Bin("lep0_pt", "Leading lepton $p_{T}$ [GeV]", 50, 0, 500),
                                hist.Bin("lep1_pt", "Trailing lepton $p_{T}$ [GeV]", 50, 0, 500),
                                )
 hists['zMass'] = spark.sparkContext.accumulator(
-    hist.Hist("Events", dataset, channel_cat,
+    hist.Hist("Events", dataset_axis, channel_cat_axis,
               hist.Bin("mass", "$m_{\ell\ell}$ [GeV]", 120, 0, 120),
               ),
     FLNAL_Hist_AccumulatorParam()
@@ -61,6 +63,7 @@ class NumpyVectorAccumulatorParam(AccumulatorParam):
 spark.conf.set("spark.sql.execution.arrow.enabled", "true")
 spark.conf.set("spark.sql.execution.arrow.fallback.enabled", "false")
 df = spark.read.parquet("file:/Users/bengal1/dev/IRIS-HEP/data/bignano.parquet")
+df = df.withColumn("dataset", functions.lit("my_dataset"))
 slim = df.select(df.event,
                  df.nElectron,
                  df.Electron_pt,
@@ -76,7 +79,8 @@ slim = df.select(df.event,
                  df.Muon_mass,
                  df.Muon_tightId.cast("array<int >"),
                  df.Muon_pdgId,
-                 df.Muon_pfRelIso04_all
+                 df.Muon_pfRelIso04_all,
+                 df.dataset
                  )
 
 
@@ -109,7 +113,11 @@ histograms = {
     },
 }
 
-def compute_zpeak(Electron_pt,
+
+
+
+def compute_zpeak(dataset,
+                  Electron_pt,
                   Electron_eta,
                   Electron_phi,
                   Electron_mass,
@@ -123,9 +131,9 @@ def compute_zpeak(Electron_pt,
                   Muon_tightId,
                   Muon_pdgId,
                   Muon_pfRelIso04_all):
-    global histograms, hist, dataset, channel_cat
+    global histograms, hist, dataset_axis, channel_cat_axis
 
-    electrons = awk.JaggedArray.zip(
+    almost_electrons = awk.JaggedArray.zip(
         {"pt": awk.JaggedArray.fromiter(Electron_pt),
          "eta": awk.JaggedArray.fromiter(Electron_eta),
          "phi": awk.JaggedArray.fromiter(Electron_phi),
@@ -135,11 +143,21 @@ def compute_zpeak(Electron_pt,
          "pfRelIso03_all": awk.JaggedArray.fromiter(Electron_pfRelIso03_all)
     })
 
-    ele = electrons[(electrons["pt"] > 20) &
-                    (np.abs(electrons["eta"]) < 2.5) &
-                    (electrons["cutBased"] >= 4)]
+    electrons = JaggedCandidateArray.candidatesfromoffsets(almost_electrons.offsets,
+                                                           pt=almost_electrons["pt"].content,
+                                                           eta=almost_electrons["eta"].content,
+                                                           phi=almost_electrons["phi"].content,
+                                                           mass=almost_electrons["mass"].content,
+                                                           cutBased=almost_electrons["cutBased"].content,
+                                                           pdgId=almost_electrons["pdgId"].content,
+                                                           pfRelIso03_all=almost_electrons["pfRelIso03_all"].content
+                                                           )
 
-    muons = awk.JaggedArray.zip(
+    ele = electrons[(electrons.pt > 20) &
+                    (np.abs(electrons.eta) < 2.5) &
+                    (electrons.cutBased >= 4)]
+
+    almost_muons = awk.JaggedArray.zip(
         {
             "pt": awk.JaggedArray.fromiter(Muon_pt),
             "eta": awk.JaggedArray.fromiter(Muon_eta),
@@ -150,9 +168,27 @@ def compute_zpeak(Electron_pt,
             "pfRelIso04_all": awk.JaggedArray.fromiter(Muon_pfRelIso04_all)
         })
 
-    mu = muons[(muons["pt"] > 20) &
-               (np.abs(muons["eta"]) < 2.4) &
-               (muons["tightId"] > 0)]
+    muons = JaggedCandidateArray.candidatesfromoffsets(almost_muons.offsets,
+                                                       pt=almost_muons[
+                                                           "pt"].content,
+                                                       eta=almost_muons[
+                                                           "eta"].content,
+                                                       phi=almost_muons[
+                                                           "phi"].content,
+                                                       mass=almost_muons[
+                                                           "mass"].content,
+                                                       tightId=
+                                                       almost_muons[
+                                                           "tightId"].content,
+                                                       pdgId=almost_muons[
+                                                           "pdgId"].content,
+                                                       pfRelIso04_all=
+                                                       almost_muons[
+                                                           "pfRelIso04_all"].content)
+
+    mu = muons[(muons.pt > 20) &
+               (np.abs(muons.eta) < 2.4) &
+               (muons.tightId > 0)]
 
     ee = ele.distincts()
     mm = mu.distincts()
@@ -160,9 +196,9 @@ def compute_zpeak(Electron_pt,
 
     dileptons = {}
     dileptons['ee'] = ee[
-        (ee.i0["pdgId"] * ee.i1["pdgId"] == -11 * 11) & (ee.i0["pt"] > 25)]
-    dileptons['mm'] = mm[(mm.i0["pdgId"] * mm.i1["pdgId"] == -13 * 13)]
-    dileptons['em'] = em[(em.i0["pdgId"] * em.i1["pdgId"] == -11 * 13)]
+        (ee.i0.pdgId * ee.i1.pdgId == -11 * 11) & (ee.i0.pt > 25)]
+    dileptons['mm'] = mm[(mm.i0.pdgId * mm.i1.pdgId == -13 * 13)]
+    dileptons['em'] = em[(em.i0.pdgId * em.i1.pdgId == -11 * 13)]
 
     channels = {}
     channels['ee'] = (ee.counts == 1) & (mu.counts == 0)
@@ -182,14 +218,13 @@ def compute_zpeak(Electron_pt,
 
         zMassHist = hists["zMass"]
 
-        zMass = hist.Hist("Events", dataset, channel_cat,
+        zMass = hist.Hist("Events", dataset_axis, channel_cat_axis,
                           hist.Bin("mass", "$m_{\ell\ell}$ [GeV]", 120, 0, 120),
                           )
 
-
-        zMass.fill(dataset=dataset, channel=channel,
-                  mass=zcands.i0["mass"].flatten(),
-                  weight=weight.flatten())
+        zMass.fill(dataset=dataset[0], channel=channel,
+                   mass=zcands.mass.flatten(),
+                   weight=weight.flatten())
         zMassHist.add(zMass)
 
         # zMassHist["accumulator"].add(connect_numpy.fromnumpy(zMass))
@@ -199,8 +234,54 @@ def compute_zpeak(Electron_pt,
 # foo_udf = pandas_udf(foo, IntegerType(), PandasUDFType.SCALAR)
 # slim.select(foo_udf("Electron_pt")).describe()
 #
+
+def foo(dataset,
+        nElectron,
+        Electron_pt,
+        Electron_eta,
+        Electron_phi,
+        Electron_mass,
+        Electron_cutBased,
+        Electron_pdgId,
+        Electron_pfRelIso03_all):
+    import sys
+    from fnal_column_analysis_tools.analysis_objects import JaggedCandidateArray
+
+    almost_electrons = awk.JaggedArray.zip(
+        {"pt": awk.JaggedArray.fromiter(Electron_pt),
+         "eta": awk.JaggedArray.fromiter(Electron_eta),
+         "phi": awk.JaggedArray.fromiter(Electron_phi),
+         "mass": awk.JaggedArray.fromiter(Electron_mass),
+         "cutBased": awk.JaggedArray.fromiter(Electron_cutBased),
+         "pdgId": awk.JaggedArray.fromiter(Electron_pdgId),
+         "pfRelIso03_all": awk.JaggedArray.fromiter(Electron_pfRelIso03_all)
+    })
+    electrons = JaggedCandidateArray.candidatesfromoffsets(almost_electrons.offsets,
+                                                           pt=almost_electrons["pt"].content,
+                                                           eta=almost_electrons["eta"].content,
+                                                           phi=almost_electrons["phi"].content,
+                                                           mass=almost_electrons["mass"].content,
+                                                           cutBased=almost_electrons["cutBased"].content,
+                                                           pdgId=almost_electrons["pdgId"].content,
+                                                           pfRelIso03_all=almost_electrons["pfRelIso03_all"].content
+                                                           )
+    return nElectron
+
+
+foo_udf = pandas_udf(foo, IntegerType(), PandasUDFType.SCALAR)
+
+slim.select(foo_udf("dataset", "nElectron",     "Electron_pt",
+    "Electron_eta",
+    "Electron_phi",
+    "Electron_mass",
+    "Electron_cutBased",
+    "Electron_pdgId",
+    "Electron_pfRelIso03_all")).describe()
+
+
 zpeak_udf = pandas_udf(compute_zpeak, IntegerType(), PandasUDFType.SCALAR)
 numpyret2 = slim.select(zpeak_udf(
+    "dataset",
     "Electron_pt",
     "Electron_eta",
     "Electron_phi",
@@ -219,6 +300,6 @@ numpyret2 = slim.select(zpeak_udf(
 # This triggers the accumulators
 numpyret2.describe()
 
-print(histograms["zMass"]["accumulator"].value.dump())
+print(hists["zMass"].value.values())
 
 

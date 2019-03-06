7d1cfda77b207d24cc5a4ab4713f14d71a783fec
Ben Galewsky <bengal1@illinois.ed>
2019-03-06 11:16:50 -0600

    Clean up ZPeak Demo

--- demo/play.py
+++ demo/play.py
@@ -16,6 +16,7 @@ from pyspark.sql.types import IntegerType, DoubleType
 from fnal_column_analysis_tools.analysis_objects import JaggedCandidateArray
 
 
+# Implement accumulatorparam class for FNAL Hists
 class FLNAL_Hist_AccumulatorParam(AccumulatorParam):
     def zero(self, value):
         return value
@@ -25,67 +26,26 @@ class FLNAL_Hist_AccumulatorParam(AccumulatorParam):
         return val1
 
 
+# Create a histogram accumulator for ZMass
 hists = OrderedDict()
 
 dataset_axis = hist.Cat("dataset", "DAS name")
 channel_cat_axis = hist.Cat("channel", "dilepton flavor")
 
-# underflow = negative weight sum, overflow = positive weight sum
-hists['genw'] = hist.Hist("Events", dataset_axis, hist.Bin("genw", "Gen weight", [0.]))
-
-hists['lepton_pt'] = hist.Hist("Events", dataset_axis, channel_cat_axis,
-                               hist.Bin("lep0_pt", "Leading lepton $p_{T}$ [GeV]", 50, 0, 500),
-                               hist.Bin("lep1_pt", "Trailing lepton $p_{T}$ [GeV]", 50, 0, 500),
-                               )
-hists['zMass'] = spark.sparkContext.accumulator(
-    hist.Hist("Events", dataset_axis, channel_cat_axis,
-              hist.Bin("mass", "$m_{\ell\ell}$ [GeV]", 120, 0, 120),
-              ),
-    FLNAL_Hist_AccumulatorParam()
-)
-
-hists['profile'] = hist.Hist("RowGroups",
-                    hist.Cat("op", "Operation", sorting='placement'),
-                    hist.Bin("dt", "$\Delta t$ [$\mu s$]", 100, 0, 10),
-                   )
+hists['zMass'] = {
+    "accumulator":
+        spark.sparkContext.accumulator(
+            hist.Hist("Events", dataset_axis, channel_cat_axis,
+                      hist.Bin("mass", "$m_{\ell\ell}$ [GeV]", 120, 0, 120),
+                      ),
+            FLNAL_Hist_AccumulatorParam()
+        ),
+    "dataset_axis": hist.Cat("dataset", "DAS name"),
+    "channel_cat_axis": hist.Cat("channel", "dilepton flavor")
+}
 
 
-
-class NumpyVectorAccumulatorParam(AccumulatorParam):
-    def zero(self, value):
-        return value
-
-    def addInPlace(self, val1, val2):
-        val1 += val2
-        return val1
-
-
-spark.conf.set("spark.sql.execution.arrow.enabled", "true")
-spark.conf.set("spark.sql.execution.arrow.fallback.enabled", "false")
-df = spark.read.parquet("file:/Users/ncsmith/src/spark-hep-query/demo/bignano.parquet")
-df = df.withColumn("dataset", functions.lit("my_dataset"))
-slim = df.select(df.event,
-                 df.nElectron,
-                 df.Electron_pt,
-                 df.Electron_eta,
-                 df.Electron_phi,
-                 df.Electron_mass,
-                 df.Electron_cutBased,
-                 df.Electron_pdgId,
-                 df.Electron_pfRelIso03_all,
-                 df.nMuon,
-                 df.Muon_pt,
-                 df.Muon_eta,
-                 df.Muon_phi,
-                 df.Muon_mass,
-                 df.Muon_tightId.cast("array<int >"),
-                 df.Muon_pdgId,
-                 df.Muon_pfRelIso04_all,
-                 df.dataset
-                 )
-
-
-
+# ZPeak UDF that reduces data to a zMass histogram
 def compute_zpeak(dataset,
                   nElectron, 
                   Electron_pt,
@@ -103,7 +63,8 @@ def compute_zpeak(dataset,
                   Muon_tightId,
                   Muon_pdgId,
                   Muon_pfRelIso04_all):
-    global hist, dataset_axis, channel_cat_axis
+
+    global hists
     tic = time.time()
 
     electrons = JaggedCandidateArray.candidatesfromcounts(
@@ -163,9 +124,10 @@ def compute_zpeak(dataset,
         tot += cut.sum()
         weight = np.array(1.)
 
-        zMassHist = hists["zMass"]
+        zMassHist = hists["zMass"]["accumulator"]
 
-        zMass = hist.Hist("Events", dataset_axis, channel_cat_axis,
+        zMass = hist.Hist("Events", hists["zMass"]["dataset_axis"],
+                          hists["zMass"]["channel_cat_axis"],
                           hist.Bin("mass", "$m_{\ell\ell}$ [GeV]", 120, 0, 120),
                           )
 
@@ -178,56 +140,33 @@ def compute_zpeak(dataset,
     dt = time.time() - tic
     return pd.Series(np.ones(Electron_pt.size) * dt/Electron_pt.size)
 
-# foo_udf = pandas_udf(foo, IntegerType(), PandasUDFType.SCALAR)
-# slim.select(foo_udf("Electron_pt")).describe()
-#
-
-def foo(dataset,
-        nElectron,
-        Electron_pt,
-        Electron_eta,
-        Electron_phi,
-        Electron_mass,
-        Electron_cutBased,
-        Electron_pdgId,
-        Electron_pfRelIso03_all):
-    import sys
-    from fnal_column_analysis_tools.analysis_objects import JaggedCandidateArray
-
-    almost_electrons = awk.JaggedArray.zip(
-        {"pt": awk.JaggedArray.fromiter(Electron_pt),
-         "eta": awk.JaggedArray.fromiter(Electron_eta),
-         "phi": awk.JaggedArray.fromiter(Electron_phi),
-         "mass": awk.JaggedArray.fromiter(Electron_mass),
-         "cutBased": awk.JaggedArray.fromiter(Electron_cutBased),
-         "pdgId": awk.JaggedArray.fromiter(Electron_pdgId),
-         "pfRelIso03_all": awk.JaggedArray.fromiter(Electron_pfRelIso03_all)
-    })
-    electrons = JaggedCandidateArray.candidatesfromoffsets(almost_electrons.offsets,
-                                                           pt=almost_electrons["pt"].content,
-                                                           eta=almost_electrons["eta"].content,
-                                                           phi=almost_electrons["phi"].content,
-                                                           mass=almost_electrons["mass"].content,
-                                                           cutBased=almost_electrons["cutBased"].content,
-                                                           pdgId=almost_electrons["pdgId"].content,
-                                                           pfRelIso03_all=almost_electrons["pfRelIso03_all"].content
-                                                           )
-    return nElectron
-
-
-foo_udf = pandas_udf(foo, DoubleType(), PandasUDFType.SCALAR)
-
-slim.select(foo_udf("dataset", "nElectron",     "Electron_pt",
-    "Electron_eta",
-    "Electron_phi",
-    "Electron_mass",
-    "Electron_cutBased",
-    "Electron_pdgId",
-    "Electron_pfRelIso03_all")).describe()
-
-
 zpeak_udf = pandas_udf(compute_zpeak, DoubleType(), PandasUDFType.SCALAR)
-numpyret2 = slim.select(zpeak_udf(
+
+spark.conf.set("spark.sql.execution.arrow.enabled", "true")
+spark.conf.set("spark.sql.execution.arrow.fallback.enabled", "false")
+df = spark.read.parquet("file:/Users/bengal1/dev/IRIS-HEP/data/bignano.parquet")
+df = df.withColumn("dataset", functions.lit("my_dataset"))
+slim = df.select(df.event,
+                 df.nElectron,
+                 df.Electron_pt,
+                 df.Electron_eta,
+                 df.Electron_phi,
+                 df.Electron_mass,
+                 df.Electron_cutBased,
+                 df.Electron_pdgId,
+                 df.Electron_pfRelIso03_all,
+                 df.nMuon,
+                 df.Muon_pt,
+                 df.Muon_eta,
+                 df.Muon_phi,
+                 df.Muon_mass,
+                 df.Muon_tightId.cast("array<int >"),
+                 df.Muon_pdgId,
+                 df.Muon_pfRelIso04_all,
+                 df.dataset
+                 )
+
+analysis = slim.select(zpeak_udf(
     "dataset",
     "nElectron",
     "Electron_pt",
@@ -247,9 +186,6 @@ numpyret2 = slim.select(zpeak_udf(
     "Muon_pfRelIso04_all"))
 
 # This triggers the accumulators
-print(numpyret2.describe().toPandas())
+print(analysis.describe().toPandas())
 
-print(hists["zMass"].value.values())
-
-#slim.write.parquet("slim")
-
+print(hists["zMass"]["accumulator"].value.values())
